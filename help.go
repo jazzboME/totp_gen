2b251291c2df46bcb046751a025533407e092ee1
Kevin Foss <kfoss@maine.edu>
2024-08-13 17:26:27 -0400

    Adjustments to help screen text

--- help.go
+++ help.go
@@ -2,5 +2,6 @@ package main
 
 var helpText string = "# Adding Sites\n\n" + 
 "To add sites, you should enter both a unique name to remember the site, i.e. 'CRM Production' " +
-"and the secret provided by the site. This will often be presented as a code under the QR code " +
-"or the code will be available via a link."
+"and the secret provided by the site.\n\n" +
+"This will often be presented as a code under the QR code " +
+"or the code will be available via a link labeled something like 'I Can't use a QR Code"
